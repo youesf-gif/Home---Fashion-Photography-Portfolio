62ac0807cddcb99347b8721e912337cda1e5f7d9
Joo_white <youssef.gabr9988@gmail.com>
2026-02-06 01:15:31 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+const btnNav = document.querySelector("button");
+
+let flag = false;
+
+btnNav.addEventListener("click", function () {
+    if (!flag) {
+        document.querySelector("nav").style.display = "block";
+        flag = true;
+    } else {
+        document.querySelector("nav").style.display = "none";
+        flag = false;
+    }
+});
